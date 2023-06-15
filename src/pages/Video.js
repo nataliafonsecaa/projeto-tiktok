1e48db969a1d423a3d046f79dc81c2c4f6a01d36
nataliafonsecaa <natalia.fonseca8@etec.sp.gov.br>
2023-06-15 20:09:59 -0300

    atualizado

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import "./Video.css"
+import Vd2 from '../videos/Download.mp4'
 
 function Video() {
   return (
     <div className='video'>
-      <video
+      {/* <video
       className='video_player'
       controls
-      src="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appsot.com/o/brecker2.mp4?alt=media&token=b5399418-9276-4e53-a706-1e00290c2c74"
+      src="https://www.youtube.com/watch?v=Jwy1yCJjNEA"
          >
-      </video>
+      </video> */}
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Jwy1yCJjNEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <video controls autostart autoPlay src={Vd2} type="video/mp4" />
+
     </div>
   )
 }
