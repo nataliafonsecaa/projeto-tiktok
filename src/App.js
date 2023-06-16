b5c8bb5ce04bd4f91973452cb9eae4b53a84b9d4
nataliafonsecaa <natalia.fonseca8@etec.sp.gov.br>
2023-06-16 13:14:53 -0300

    aula2
    aula 2 completa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Video from "./pages/Video.js"
+import Video from './pages/Video';
 
 function App(){
   return(
     <div className='App'>
       <div className='app_videos'>
-      <Video />
-      <Video />
       <Video />
       <Video />
       </div>
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import "./Video.css"
-import Vd2 from '../videos/Download.mp4'
+import React, { useRef, useState } from "react";
+import "./Video.css";
+import vD from "../videos/Download.mp4";
 
 function Video() {
-  return (
-    <div className='video'>
-      {/* <video
-      className='video_player'
-      controls
-      src="https://www.youtube.com/watch?v=Jwy1yCJjNEA"
-         >
-      </video> */}
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/Jwy1yCJjNEA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      <video controls autostart autoPlay src={Vd2} type="video/mp4" />
+  const videoRef = useRef(null);
+  const [ play, setPlay ] = useState(false);
 
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+  return (
+    <div className="video">
+      <video
+        className="video_player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src={vD} type="video/mp4"
+      ></video>
     </div>
-  )
+  );
 }
 
-export default Video
+export default Video;
